dd4af6199faa041a89052f5a149779e5c5d0d695
EC2 Default User <ec2-user@ip-172-31-17-199.ec2.internal>
2021-02-10 22:43:02 +0000

    マイページにてログイン中のユーザー名を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!
     def index
-        @user = User.find(params[:id])
+        @user = current_user.name
     end
 end
