058182d623a05fae6bdf6484097bbd1f0a521291
EC2 Default User <ec2-user@ip-172-31-17-199.ec2.internal>
2021-02-11 00:01:18 +0000

    編集画面の作成（途中）、リダイレクト先の確認から

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
     def index
-        @user = current_user.name
+    end
+    
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def update
+        @user = User.find(params[:id])
+        @user.update
+        redirect_to "/"
     end
 end
