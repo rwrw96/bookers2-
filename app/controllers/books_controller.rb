d93cb6c213087beef098f83f77316eb01fe77f98
EC2 Default User <ec2-user@ip-172-31-17-199.ec2.internal>
2021-02-11 23:08:33 +0000

    投稿したデータをTOPページに表示するように修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,16 @@
 class BooksController < ApplicationController
     
-    # def new
-    #     @book = Book.new
-    #     @books = Book.all
-    # end
+    def new
+        @book = Book.new(book_params)
+        @books = Book.all
+    end
     
     def index
-        @book = Book.new
         @books = Book.all
     end
     
     def create
+        @books = Book.all
         book = Book.new(book_params)
         book.save
         redirect_to "/"
@@ -18,6 +18,6 @@ class BooksController < ApplicationController
     
     private
     def book_params
-       params.require(:book).permit(:title, :body)    
+       params.permit(:title, :body)    
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "homes#top"
+  root to: "books#index"
   resource :books
   
   post "/" => "books#create"
   get "/" => "books#index"
   get "/" => "users#index"
-  # get "books/index" => "books#index"
+  get "users/edit" => "users#edit"
+  patch "users/edit" => "users/edit"
+  post "/" => "books#new"
+  post "/" => "books#create"
+  get "books/index" => "books#index"
 end
