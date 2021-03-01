feab84bf797f139a8392b802888717c94e0bd492
EC2 Default User <ec2-user@ip-172-31-17-199.ec2.internal>
2021-03-01 13:56:12 +0000

    migrateの整理

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_21_082710) do
+ActiveRecord::Schema.define(version: 2021_02_10_210716) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,12 +26,13 @@ ActiveRecord::Schema.define(version: 2021_02_21_082710) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.text "introduction"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "introduction", default: "", null: false
     t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
